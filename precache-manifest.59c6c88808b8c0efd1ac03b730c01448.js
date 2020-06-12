94a396fe499a70e6838bf7ce4b85ccb855b72db2
Doh09 <alexandreasen09@gmail.com>
2020-06-12 22:23:38 +0200

    Updates

---
+++ precache-manifest.59c6c88808b8c0efd1ac03b730c01448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92907356c1b1acd7d673bb9dbd1204d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9c2c0272699ebed7fee2",
+    "url": "/monsters-rolodex/static/css/main.c05017a1.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c2c0272699ebed7fee2",
+    "url": "/monsters-rolodex/static/js/main.e54a2d4f.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
